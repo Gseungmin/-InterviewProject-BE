237023d85f017c9253b7e4b2323ac306aa64f74a
Ji Seungmin <cswcsm02@gmail.com>
2022-07-14 12:32:53 +0900

    Cor Error 해결 + Mysql 연동 및 AWS 배포

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,10 +1,10 @@
 -- 임베디드 DB 사용을 위한 기본 테이블 생성
 drop table if exists orders CASCADE;
 create table orders (
-    order_id bigint not null,
-    email varchar(255),
-    phone_number varchar(255),
-    order_date varchar(255),
-    username varchar(255),
-    primary key (order_id)
-)
+                        order_id bigint not null,
+                        email varchar(255),
+                        order_date varchar(255),
+                        phone_number varchar(255),
+                        username varchar(255),
+                        primary key (order_id)
+) engine=InnoDB
